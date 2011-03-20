9961de2afd72c6e3d03f52711d1b22c4c56828bb
Guillaume Emont <gemont@igalia.com>
2011-03-20 01:13:52 +0100

    Added function to convert a GstBuf into an IplImage

--- python/gst_virtual_tripod.py
+++ python/gst_virtual_tripod.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import glib,gobject, gst
 
+import cv
+
 
 class VirtualTripod(gst.Element):
     __gstdetails__ = ("virtual tripod",
@@ -30,6 +32,22 @@ class VirtualTripod(gst.Element):
         self.srcpad = gst.Pad(self.src_template)
         self.add_pad(self.srcpad)
 
+
+    def _buf_to_cv_img(self, buf):
+        # cv.IPL_DEPTH_16S  cv.IPL_DEPTH_16U  cv.IPL_DEPTH_32F
+        # cv.IPL_DEPTH_32S  cv.IPL_DEPTH_64F  cv.IPL_DEPTH_8S   cv.IPL_DEPTH_8U
+        # cv.IPL_ORIGIN_BL  cv.IPL_ORIGIN_TL
+        depth_conversion = {8: cv.IPL_DEPTH_8U, 16: cv.IPL_DEPTH_16U}
+        struct = buf.caps[0]
+        width = struct['width']
+        height = struct['height']
+        channels = 3 # we only accept x-raw-rgb
+        depth = struct['depth'] / channels
+        img = cv.CreateImageHeader((width, height), depth_conversion[depth],
+                                   channels);
+        cv.SetData (img, buf.data)
+
+        return img
 
 
     def chain(self, pad, buf):
