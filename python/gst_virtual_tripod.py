676ed0619e97a350980657818a62f1162cb0fdb7
Guillaume Emont <guijemont@igalia.com>
2011-06-13 17:48:25 +0200

    Better optical flow by trying to remove displacement vectors that diverge too much from overl transform

--- python/gst_virtual_tripod.py
+++ python/gst_virtual_tripod.py
@@ -3,6 +3,8 @@ import glib,gobject,gst,sys
 
 from itertools import izip
 
+import numpy, math
+
 import cv
 
 SHAPE_CROSS = 0
@@ -100,9 +102,7 @@ class OpticalFlowFinder(object):
           mat[0, i] = points[i]
         return mat
 
-    def optical_flow_perspective_transform(self, img0, img1):
-        points0, points1 = self.optical_flow(img0, img1)
-
+    def perspective_transform_from_points(self, points0, points1):
         mat0 = self._mat_of_point_list (points0)
         mat1 = self._mat_of_point_list (points1)
         transform = cv.CreateMat(3, 3, cv.CV_64F)
@@ -111,6 +111,74 @@ class OpticalFlowFinder(object):
 
         return transform
 
+    def _point_to_mat(self, point):
+        """
+        Converts a point represented in a 2-tuple into a one column, 3 rows
+        matrix representing it in homogeneous coordinates.
+        """
+        mat_res = cv.CreateMat(3, 1, cv.CV_64F)
+        mat_res[0,0] = point[0]
+        mat_res[1,0] = point[1]
+        mat_res[2,0] = 1.
+
+        return mat_res
+
+    def _apply_transform(self, transform, point):
+        #print "transform:"
+        #print numpy.asarray(transform)
+        #print "point:", point
+        pt = self._point_to_mat(point)
+        mat_res = cv.CreateMat(3, 1, cv.CV_64F)
+        cv.MatMul(transform, pt, mat_res)
+        x,y,z = tuple(mat_res[i,0] for i in xrange(3))
+        res = (x/z, y/z)
+        #print "res:", (x,y,z), res
+        return res
+
+    def _squared_distance(self, (x0, y0), (x1, y1)):
+        return (x0-x1)**2 + (y0-y1)**2
+
+    def filter_points_from_transform(self, points0, points1, transform):
+        result0, result1 = [], []
+        points = []
+        sq_dists = []
+        for p0, p1 in zip(points0, points1):
+            points.append((p0, p1))
+            # note that transform is the homography to go from 1 to 0
+            transformed_p1 = self._apply_transform(transform, p1)
+            sq_dist = self._squared_distance(transformed_p1, p0)
+            sq_dists.append(sq_dist)
+
+        avg_sq_dist = sum(sq_dists) / len(sq_dists)
+        print "average squared distance:", avg_sq_dist
+        MAX_SQ_DIST = 200.**2.
+
+        for (p0, p1), sq_dist in zip(points, sq_dists):
+            if sq_dist < MAX_SQ_DIST:
+                #print "appending %s [%s] d=%f" % ((p0, p1), transformed_p1, math.sqrt(sq_dist))
+                result0.append(p0)
+                result1.append(p1)
+            else:
+                #print "ignoring %s [%s] d=%f" % ((p0, p1), transformed_p1, math.sqrt(sq_dist))
+                pass
+
+
+        print "kept %d of %d points" % (len(result0), len(points0))
+        return result0, result1
+
+    def better_optical_flow(self, img0, img1):
+        # FIXME: try to iter over that whole thing until we have something
+        # nice? Find some way to make it stabilise?
+        points0, points1 = self.optical_flow(img0, img1)
+
+        first_transform = self.perspective_transform_from_points(points0, points1)
+
+        return self.filter_points_from_transform(points0, points1, first_transform)
+
+    def optical_flow_perspective_transform(self, img0, img1):
+        points0, points1 = self.better_optical_flow(img0, img1)
+        return self.perspective_transform_from_points(points0, points1)
+
 
 class ArrowDrawer(object):
     def draw_arrows(self, img, origins, ends):
@@ -118,6 +186,10 @@ class ArrowDrawer(object):
             self.draw_arrow(img, origin, end)
 
     def draw_arrow(self, img, origin, end):
+        def int_pos((x,y)):
+            return (int(x), int(y))
+        origin = int_pos(origin)
+        end = int_pos(end)
         cv.Line(img, origin, end, (128,), 2)
         # yeah, that's a lazy approximation of an arrow
         cv.Circle(img, end, 4, (128,), 2)
@@ -177,7 +249,7 @@ class OpticalFlowDrawer(ReferenceImageTransformer):
         self._drawer = ArrowDrawer()
 
     def transform(self, img0, img0_transformed, img1):
-        origins, ends = self._flow_finder.optical_flow(img0, img1)
+        origins, ends = self._flow_finder.better_optical_flow(img0, img1)
 
         img_result = copy_img(img1)
         self._drawer.draw_arrows(img_result, origins, ends)
@@ -233,10 +305,16 @@ class VirtualTripod(gst.Element):
     self.srcpad = gst.Pad(self.src_template)
     self.add_pad(self.srcpad)
 
+    #self._transformer = OpticalFlowDrawer()
     self._transformer = MotionCompensator()
+
+    self._seq = 0
 
 
   def chain(self, pad, buf):
+    print "##### seq:", self._seq
+    self._seq += 1
+
     img = img_of_buf(buf)
     transformed_img = self._transformer.process_image(img)
     new_buf = buf_of_img(transformed_img, bufmodel=buf)
