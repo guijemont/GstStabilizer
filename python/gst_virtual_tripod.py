8167950a76b5ea074f5f4e6a969ea6e54ade2779
Guillaume Emont <guijemont@igalia.com>
2011-09-10 14:50:28 +0200

    Added a base class to handle the remuxing of optical flow information and original images

--- python/gst_virtual_tripod.py
+++ python/gst_virtual_tripod.py
@@ -6,6 +6,7 @@ from itertools import izip
 import numpy, math
 
 import cPickle
+from collections import deque
 
 import cv
 
@@ -242,6 +243,60 @@ class OpticalFlowFinder(gst.Element):
     def optical_flow_perspective_transform(self, img0, img1):
         points0, points1 = self.better_optical_flow(img0, img1)
         return self.perspective_transform_from_points(points0, points1)
+
+
+class OpticalFlowMuxer(gst.Element):
+    """
+    Base class for "muxers" of an optical flow stream and another stream having
+    the same timestamps.
+    When subclassing, you should implement mux and redefine main_sink_template
+    """
+
+    flow_sink_template = gst.PadTemplate ("flowsink",
+                                           gst.PAD_SINK,
+                                           gst.PAD_ALWAYS,
+                                           gst.Caps('application/x-motion-flow'))
+
+    # should be defined as a proper pad template in the subclass
+    main_sink_template = None
+
+    def __init__(self):
+        gst.Element.__init__(self)
+
+        self.flow_sink_pad = gst.Pad(self.flow_sink_template)
+        self.flow_sink_pad.set_chain_function(self._flow_chain)
+        self.add_pad(self.flow_sink_pad)
+
+        self.main_sink_pad = gst.Pad(self.main_sink_template)
+        self.main_sink_pad.set_chain_function(self._main_chain)
+        self.add_pad(self.main_sink_pad)
+
+        # FIXME: shouldn't we just use gstreamer queues outside of the elemnt?
+        self._pending_flow = deque()
+        self._pending_main = deque()
+
+    def mux(self, buf, flow):
+        raise NotImplementedError("This method needs to be implemented in a subclass")
+
+    def _chain(self, pad, buf):
+        if pad == self.flow_sink_pad:
+            self._pending_flow.append(buf)
+        else: # self.main_sink_pad
+            self._pending_main.append(buf)
+        return self._try_mux()
+
+    def _try_mux(self):
+        if self._pending_flow and self._pending_main:
+            flow_buf = self._pending_flow.popleft()
+            flow = cPickle.loads(flow_buf.data)
+            buf = self._pending_main.popleft()
+            if buf.timestamp == flow_buf.timestamp:
+                return mux(buf, flow)
+            else:
+                print "All going wrong!"
+                return gst.FLOW_ERROR
+
+        return gst.FLOW_OK
 
 
 class ArrowDrawer(object):
