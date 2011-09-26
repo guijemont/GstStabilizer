061b39ca25be401021c2bb19ab3251719fa70b80
Guillaume Emont <guijemont@igalia.com>
2011-09-26 23:05:30 +0200

    remove plenty of unused code

--- python/gst_virtual_tripod.py
+++ python/gst_virtual_tripod.py
@@ -10,11 +10,6 @@ from collections import deque
 
 import cv
 
-SHAPE_CROSS = 0
-SHAPE_BOX = 1
-SHAPE_CIRCLE = 2
-
-SHAPE_MODULO = 3
 
 MAX_COUNT = 20
 
@@ -57,10 +52,6 @@ def buf_of_img(img, bufmodel=None):
         buf.offset_end = bufmodel.offset_end
     return buf
 
-def copy_img(img):
-    new_img = cv.CreateImageHeader((img.width, img.height), 8, 1)
-    cv.SetData(new_img, img.tostring())
-    return new_img
 
 class OpticalFlowFinder(gst.Element):
     __gstdetails__ = ("Optical flow finder",
@@ -129,19 +120,17 @@ class OpticalFlowFinder(gst.Element):
         return [ p for (status,p, err) in zip(flter, features, errors)
                     if status] # and err < admissible_error]
 
-    def optical_flow (self, buf0, buf1):
-        img0 = img_of_buf (buf0)
-        img1 = img_of_buf (buf1)
-        return self.img_optical_flow(img0, img1)
-
-    def img_optical_flow(self, img0, img1):
+    def optical_flow(self, buf0, buf1):
         """
-        Return two sets of coordinates (c0, c1), in img0 and img1 respectively,
-        such that c1[i] is the position in img1 of the feature that is at c0[i]
-        in img0.
+        Return two sets of coordinates (c0, c1), in buf0 and buf1 respectively,
+        such that c1[i] is the position in buf1 of the feature that is at c0[i]
+        in buf0.
         """
-        if img0 is None:
+        if buf0 is None:
             return None
+
+        img0 = img_of_buf(buf0)
+        img1 = img_of_buf(buf1)
 
         corners0 = self._features(img0)
 
@@ -164,90 +153,6 @@ class OpticalFlowFinder(gst.Element):
 
         return (corners0, corners1)
 
-    def _mat_of_point_list(self, points):
-        n = len(points)
-        mat = cv.CreateMat(1, n, cv.CV_32FC2)
-        for i in xrange(n):
-          mat[0, i] = points[i]
-        return mat
-
-    def perspective_transform_from_points(self, points0, points1):
-        mat0 = self._mat_of_point_list (points0)
-        mat1 = self._mat_of_point_list (points1)
-        transform = cv.CreateMat(3, 3, cv.CV_64F)
-
-        cv.FindHomography(mat0, mat1, transform)
-
-        return transform
-
-    def _point_to_mat(self, point):
-        """
-        Converts a point represented in a 2-tuple into a one column, 3 rows
-        matrix representing it in homogeneous coordinates.
-        """
-        mat_res = cv.CreateMat(3, 1, cv.CV_64F)
-        mat_res[0,0] = point[0]
-        mat_res[1,0] = point[1]
-        mat_res[2,0] = 1.
-
-        return mat_res
-
-    def _apply_transform(self, transform, point):
-        #print "transform:"
-        #print numpy.asarray(transform)
-        #print "point:", point
-        pt = self._point_to_mat(point)
-        mat_res = cv.CreateMat(3, 1, cv.CV_64F)
-        cv.MatMul(transform, pt, mat_res)
-        x,y,z = tuple(mat_res[i,0] for i in xrange(3))
-        res = (x/z, y/z)
-        #print "res:", (x,y,z), res
-        return res
-
-    def _squared_distance(self, (x0, y0), (x1, y1)):
-        return (x0-x1)**2 + (y0-y1)**2
-
-    def filter_points_from_transform(self, points0, points1, transform):
-        result0, result1 = [], []
-        points = []
-        sq_dists = []
-        for p0, p1 in zip(points0, points1):
-            points.append((p0, p1))
-            # note that transform is the homography to go from 1 to 0
-            transformed_p1 = self._apply_transform(transform, p1)
-            sq_dist = self._squared_distance(transformed_p1, p0)
-            sq_dists.append(sq_dist)
-
-        avg_sq_dist = sum(sq_dists) / len(sq_dists)
-        print "average squared distance:", avg_sq_dist
-        MAX_SQ_DIST = 200.**2.
-
-        for (p0, p1), sq_dist in zip(points, sq_dists):
-            if sq_dist < MAX_SQ_DIST:
-                #print "appending %s [%s] d=%f" % ((p0, p1), transformed_p1, math.sqrt(sq_dist))
-                result0.append(p0)
-                result1.append(p1)
-            else:
-                #print "ignoring %s [%s] d=%f" % ((p0, p1), transformed_p1, math.sqrt(sq_dist))
-                pass
-
-
-        print "kept %d of %d points" % (len(result0), len(points0))
-        return result0, result1
-
-    def better_optical_flow(self, img0, img1):
-        # FIXME: try to iter over that whole thing until we have something
-        # nice? Find some way to make it stabilise?
-        points0, points1 = self.optical_flow(img0, img1)
-        return points0, points1
-
-        first_transform = self.perspective_transform_from_points(points0, points1)
-
-        return self.filter_points_from_transform(points0, points1, first_transform)
-
-    def optical_flow_perspective_transform(self, img0, img1):
-        points0, points1 = self.better_optical_flow(img0, img1)
-        return self.perspective_transform_from_points(points0, points1)
 
 gobject.type_register (OpticalFlowFinder)
 ret = gst.element_register (OpticalFlowFinder, 'opticalflowfinder')
@@ -343,53 +248,6 @@ class ArrowDrawer(object):
         return ((int(xc), int(yc)), (int(xd), int(yd)))
 
 
-class SuccessiveImageTransformer(object):
-    """
-    Apply transformations on images that depend on the previous image (either
-    raw or transformed).
-    You want to subclass transform().
-
-    If reference_is_first is True, the transformation will be made compared to
-    the first image of the series, not the previous one.
-    """
-    reference_is_first = False
-    def __init__(self, *args, **kw):
-        super(SuccessiveImageTransformer, self).__init__(*args, **kw)
-        self._img0 = None
-        self._img0_transformed = None
-
-    def process_image(self, img):
-        """
-        Will return img, as (optionally) transformed by transform(), and will
-        keep a reference on both img and its transformed value until the next
-        call to process_image().
-        """
-        if self._img0:
-            img_transformed = self.transform(self._img0,
-                                             self._img0_transformed,
-                                             img)
-        else:
-            img_transformed = None
-
-        if self._img0 is None or self.reference_is_first == False:
-            self._img0, self._img0_transformed = img, img_transformed
-
-        if img_transformed:
-            return img_transformed
-        else:
-            return img
-
-    def transform(self, img0, img0_transformed, img1):
-        """
-        At this point, img0 and img0_transformed are the previous
-        image before and after transformation (the latter might be None),
-        img1 is the new image, and you should return a transformed version
-        """
-        raise NotImplementedError("transform() needs to be implemented in a subclass")
-
-class ReferenceImageTransformer(SuccessiveImageTransformer):
-    reference_is_first = True
-
 class OpticalFlowDrawer(OpticalFlowMuxer):
     __gstdetails__ = ("Optical flow drawer",
                     "Filter/Video",
@@ -434,85 +292,3 @@ class OpticalFlowDrawer(OpticalFlowMuxer):
 
 gobject.type_register (OpticalFlowDrawer)
 ret = gst.element_register (OpticalFlowDrawer, 'opticalflowdrawer')
-
-
-class MotionCompensator(ReferenceImageTransformer):
-    def __init__(self, *args, **kw):
-        super(MotionCompensator, self).__init__(*args, **kw)
-        self._flow_finder = OpticalFlowFinder()
-
-    def warp(self, img, perspective_transform):
-        new_img = cv.CreateImage((img.width, img.height), 8, 1)
-        cv.WarpPerspective(img, new_img,
-                           perspective_transform,
-                           cv.CV_WARP_INVERSE_MAP)
-        return new_img
-
-    def transform(self, img0, img0_transformed, img1):
-        persp_transform = \
-            self._flow_finder.optical_flow_perspective_transform(img0, img1)
-
-        return self.warp(img1, persp_transform)
-
-class VirtualTripod(gst.Element):
-  __gstdetails__ = ("virtual tripod",
-                    "Filter/Video",
-                    "Align your frames",
-                    "Guillaume Emont")
-
-  sink_template = gst.PadTemplate ("sink",
-                                   gst.PAD_SINK,
-                                   gst.PAD_ALWAYS,
-                                   gst.Caps('video/x-raw-gray,depth=8'))
-
-  src_template = gst.PadTemplate ("source",
-                                     gst.PAD_SRC,
-                                     gst.PAD_ALWAYS,
-                                     gst.Caps('video/x-raw-gray,depth=8'))
-
-  __gsttemplates__ = (sink_template, src_template)
-
-  # value taken from opencv's find_obj.cpp sample
-  SECOND_SHORTEST_RATIO = 0.4
-
-  def __init__(self):
-    gst.Element.__init__(self)
-
-    self.sinkpad = gst.Pad(self.sink_template)
-    self.sinkpad.set_chain_function(self.chain)
-    self.add_pad(self.sinkpad)
-
-    self.srcpad = gst.Pad(self.src_template)
-    self.add_pad(self.srcpad)
-
-    #self._transformer = OpticalFlowDrawer()
-    self._transformer = MotionCompensator()
-
-    self._seq = 0
-
-
-  def chain(self, pad, buf):
-    print "##### seq:", self._seq
-    self._seq += 1
-
-    img = img_of_buf(buf)
-    transformed_img = self._transformer.process_image(img)
-    new_buf = buf_of_img(transformed_img, bufmodel=buf)
-
-    return self.srcpad.push(new_buf)
-
-
-gobject.type_register (VirtualTripod)
-ret = gst.element_register (VirtualTripod, 'virtualtripod')
-
-def main(args):
-  pipeline = gst.parse_launch("filesrc location=/home/guijemont/Photos/tests/test_guij.ogg ! decodebin ! ffvideoscale  ! ffmpegcolorspace ! video/x-raw-gray ! virtualtripod ! ffmpegcolorspace ! xvimagesink")
-  pipeline.set_state (gst.STATE_PLAYING)
-  gobject.MainLoop().run()
-
-if __name__ == '__main__':
-  import sys
-  glib.threads_init()
-  main(sys.argv)
-
-__gstelementfactory__ = ( "virtualtripod", gst.RANK_MARGINAL, VirtualTripod)
