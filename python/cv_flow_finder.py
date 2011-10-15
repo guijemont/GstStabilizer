7b636647440546510e643dd2fc394b80c7aa78f8
Guillaume Emont <guijemont@igalia.com>
2011-10-15 23:05:15 +0200

    Finders: SURF: filter out some bad points, all: add a demo

--- python/cv_flow_finder.py
+++ python/cv_flow_finder.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from itertools import izip
+import random
 
 import cv, cv2
 
@@ -168,20 +169,27 @@ class SURFFinder(Finder):
         # - keypoints is a list of ((x, y), laplacian, size, direction, hessian)
         # - descriptor is a cvSeq (kinda list?) of lists of 128 floats each
         #return self._surf.detect(img, None)
-        return cv.ExtractSURF(img, None, self._mem_storage, (1, 500, 3, 4))
+        if type(img) == numpy.ndarray:
+            if len(img.shape)==3 and img.shape[2] > 1:
+                img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            img = cv.fromarray(img)
+        return cv.ExtractSURF(img, None, self._mem_storage, (1, 3000, 3, 4))
 
     def optical_flow_img(self, img0, img1):
-        surf_keypoints0, surf_keypoints1 = self.matching_surf_keypoints(img0, img1)
+        surf_keypoints0, surf_keypoints1, dists = self.matching_surf_keypoints(img0, img1)
 
         print "found %d matches" % len(surf_keypoints0)
 
         def surf_to_normal_point_list(surf_kp_list):
             return [p[0] for p in surf_kp_list]
 
+        surf_keypoints0, surf_keypoints1 = self._filter_diverging_angles(surf_keypoints0,
+                                                                         surf_keypoints1)
+
         keypoints0 = surf_to_normal_point_list(surf_keypoints0)
-        keypoints0 = self._refine_points(img0, keypoints0)
+        #keypoints0 = self._refine_points(img0, keypoints0)
         keypoints1 = surf_to_normal_point_list(surf_keypoints1)
-        keypoints1 = self._refine_points(img1, keypoints1)
+        #keypoints1 = self._refine_points(img1, keypoints1)
 
         return keypoints0, keypoints1
 
@@ -191,22 +199,58 @@ class SURFFinder(Finder):
         # ((x, y), laplacian, size, dir, hessian)
 
         keypoints0, descriptors0 = self.get_surf(img0)
+        print "img0: found %d points" % len(keypoints0)
         keypoints1, descriptors1 = self.get_surf(img1)
+        print "img1: found %d points" % len(keypoints1)
 
-        indices = self._find_neighbours(descriptors0, descriptors1)
+        indices, dists = self._find_neighbours(descriptors0, descriptors1)
 
         (result_keypoints0, result_keypoints1) = ([], [])
         for idx0, idx1 in indices:
             result_keypoints0.append(keypoints0[idx0])
             result_keypoints1.append(keypoints1[idx1])
 
-        return result_keypoints0, result_keypoints1
+        return result_keypoints0, result_keypoints1, dists
+
+    def _filter_diverging_angles(self, surf_keypoints0, surf_keypoints1):
+        rotation_angles = []
+        for spt0, spt1 in izip(surf_keypoints0, surf_keypoints1):
+            dir0 = spt0[3]
+            dir1 = spt1[3]
+            angle = (dir1 - dir0) % 360
+
+            if angle > 180:
+                angle -= 360
+
+            rotation_angles.append(angle)
+
+        median = numpy.median(rotation_angles)
+
+        res0, res1 = [], []
+        dropped = 0
+        for angle, spt0, spt1 in izip(rotation_angles,
+                                      surf_keypoints0, surf_keypoints1):
+            if abs(angle-median) < 20:
+                res0.append(spt0)
+                res1.append(spt1)
+            else:
+                dropped += 1
+
+        if dropped != 0:
+            print "dropped %d points that were badly orientated" % dropped
+
+        return res0, res1
 
     def _refine_points(self, img, points):
-        return cv.FindCornerSubPix(img, points, (10, 10), (-1, -1),
-                                   (cv.CV_TERMCRIT_ITER | cv.CV_TERMCRIT_EPS,
-                                    20, 0.03))
-
+        if isinstance(img, cv.iplimage):
+            return cv.FindCornerSubPix(img, points, (10, 10), (-1, -1),
+                (cv.CV_TERMCRIT_ITER | cv.CV_TERMCRIT_EPS,
+                 20, 0.03))
+        else:
+            points = numpy.asarray(points, dtype=numpy.float32)
+            return cv2.cornerSubPix(img, points, (10, 10), (-1, -1),
+                                    (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT,
+                                     20, 0.03))
     def _find_neighbours(self, descriptors0, descriptors1):
         # return a list of pairs (idx0, idx1) such that descriptors0[idx0] is
         # very likely to describe the same feature as descriptors1[idx1]
@@ -223,10 +267,50 @@ class SURFFinder(Finder):
         # descriptors0[indices[i][0]] <-> descriptors1[i]
 
         result = []
+        result_dists = []
 
         for i, flann_idx, (small_dist, big_dist) in izip(xrange(len(needles)),
                                                          indices,
                                                          dists):
-            if small_dist < big_dist * 0.6:
+            if small_dist < 0.1 and small_dist < big_dist * 0.6:
                 result.append((flann_idx[0], i))
-        return result
+                result_dists.append(small_dist)
+        return result, result_dists
+
+
+class FinderDemo(object):
+    def __init__(self, finder, path0, path1, pathout, *args, **kw):
+        super(FinderDemo, self).__init__(*args, **kw)
+        self._finder = finder
+        self._image0 = cv2.imread(path0)
+        self._image1 = cv2.imread(path1)
+        self._pathout = pathout
+
+    def demo(self):
+        points0, points1 = self._finder.optical_flow_img(self._image0, self._image1)
+        h0, w0 = self._image0.shape[:2]
+        h1, w1 = self._image1.shape[:2]
+
+        demo_image = numpy.zeros((max(h0, h1), w0+w1, 3))
+        demo_image[:h0, :w0] = self._image0
+        demo_image[:h1, w0:w0+w1] = self._image1
+
+        for (i, (x0, y0), (x1, y1)) in izip(xrange(len(points0)), numpy.int32(points0), numpy.int32(points1)):
+            color = (random.randint(0, 255),
+                     random.randint(0, 255),
+                     random.randint(0, 255))
+
+            cv2.line(demo_image, (x0, y0), (x1+w0, y1), color)
+            cv2.putText(demo_image, str(i),
+                        (int((x0+x1+w0)/2), int((y0+y1)/2)),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        1., # font scale
+                        color)
+
+        cv2.imwrite(self._pathout, demo_image)
+
+
+if __name__ == '__main__':
+    import sys
+    demo = FinderDemo(SURFFinder(), *sys.argv[1:])
+    demo.demo()
